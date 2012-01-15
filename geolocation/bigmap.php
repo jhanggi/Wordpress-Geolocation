00e679c61634ceef1f535ca953e0d97f4850fbbf
Jason Hanggi <jason@tablexi.com>
2012-01-15 01:24:23 -0600

    don't include posts that don't have geolocation metadata

--- geolocation/bigmap.php
+++ geolocation/bigmap.php
@@ -16,11 +16,13 @@ function geolocation_map_for_posts($posts, $options) {
 	$locations = array();
 	foreach ($posts as $post) {
 		$loc = array();
-		$loc['latitude'] = clean_coordinate(get_post_meta($post->ID, 'geo_latitude', true));
-		$loc['longitude'] = clean_coordinate(get_post_meta($post->ID, 'geo_longitude', true));
-		$loc['title'] = get_post_meta($post->ID, 'geo_address', true);
-		$loc['url'] = get_permalink($post->ID);
-		array_push($locations, $loc);
+		if (get_post_meta($post->ID, 'geo_enabled') && get_post_meta($post->ID, 'geo_address', true)) {
+			$loc['latitude'] = clean_coordinate(get_post_meta($post->ID, 'geo_latitude', true));
+			$loc['longitude'] = clean_coordinate(get_post_meta($post->ID, 'geo_longitude', true));
+			$loc['title'] = get_post_meta($post->ID, 'geo_address', true);
+			$loc['url'] = get_permalink($post->ID);
+			array_push($locations, $loc);
+		}
 	}
 	$default_options = array(
 		'width' => '400px',
