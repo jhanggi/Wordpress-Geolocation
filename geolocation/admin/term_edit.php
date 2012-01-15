b67959a958dde192351e668efef7d40a544686b7
Jason Hanggi <jason@tablexi.com>
2012-01-14 23:57:00 -0600

    renamed some functions to be better namespaced

--- geolocation/admin/term_edit.php
+++ geolocation/admin/term_edit.php
@@ -1,13 +1,13 @@
 <?php 
 $types = array('category', 'post_tag');
 foreach ($types as $type) {
-	add_action($type . '_add_form_fields', 'category_metabox_add');
-	add_action($type. '_edit_form_fields', 'category_metabox_add');
-	add_action('edited_' . $type, 'category_save_fields');
-	add_action('created_' . $type, 'category_save_fields');
+	add_action($type . '_add_form_fields', 'geolocation_taxonomy_metabox_add');
+	add_action($type. '_edit_form_fields', 'geolocation_taxonomy_metabox_add');
+	add_action('edited_' . $type, 'geolocation_taxonomy_save_fields');
+	add_action('created_' . $type, 'geolocation_taxonomy_save_fields');
 }
 
-function category_metabox_add() {
+function geolocation_taxonomy_metabox_add() {
 	admin_head('tag');
 	$nonce = wp_create_nonce('geolocation_term_meta');
 	echo '<input type="hidden" id="geolocation_nonce" name="geolocation_nonce" value="' .
@@ -15,7 +15,7 @@ function category_metabox_add() {
 	include 'inner_box.php';
 }
 
-function category_save_fields($term_id) {
+function geolocation_taxonomy_save_fields($term_id) {
 	return geolocation_update_meta('term', $term_id, 'geolocation_term_meta');
 }
 ?>
