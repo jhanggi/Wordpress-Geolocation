b3d9fb8269e38b4c3882ce8cf53bfb01c6558dd0
Jason Hanggi <jason@tablexi.com>
2012-01-17 21:12:07 -0600

    ability to set default map level

--- geolocation/widget.php
+++ geolocation/widget.php
@@ -13,7 +13,9 @@ class WP_Geolocation_Widget extends WP_Simple_Widget {
 			'zoom' => array('type' => 'select', 
 							'choices' => array('Globe' => 1, 'Country' => 3, 'State' => 6, 'City' => 9),
 							'number' => true),
-			'height' => array()
+			'height' => array(),
+			'default display' => array('type' => 'select',
+										'choices' => array('All Posts' => 'all', 'Tags' => 'tags', 'Categories' => 'categories'))
 		);
 		parent::WP_Simple_Widget($widget_options, $my_options);
 	}
@@ -34,16 +36,22 @@ class WP_Geolocation_Widget extends WP_Simple_Widget {
 		$zoom = $instance['zoom'] ? $instance['zoom'] : 1;
 		
 		global $tag, $cat;
-		error_log("tags" . $tag);
-		error_log('category'.$cat);
+
 		$options = array('height' => $instance['height'], 'width' => '100%', 'zoom' => $zoom);
 		if ($tag) {
 			geolocation_map_term('tag', $tag, $options);
 		} elseif ($cat) {
 			geolocation_map_term('category', $cat, $options);
 		} else {
-			geolocation_map_all_terms('post_tag', $options);
-			//geolocation_map_all_terms('category', $options);
+			$term_enabled = function_exists('get_term_meta');
+			$disp = $instance['default display'];
+			if ($term_enabled && $disp == 'tags') {
+				geolocation_map_all_terms('post_tag', $options);
+			} elseif ($term_enabled && $disp == 'categories') {
+				geolocation_map_all_terms('category', $options);
+			} else {
+				geolocation_map_all($options);
+			}
 		}
 	}
 }
